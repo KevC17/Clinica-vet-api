ff85e49e62b1fbf4c70b06e4cbf19734c5f6e27a
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-11-26 19:36:56 -0500

    Add: daily dose and weight control exercises

--- pets/urls.py
+++ pets/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('pets/list', views.pets_get_list),
     path('pets', views.pets_post_create),
     path('pets/<int:id>/', views.pets_get_by_id),
-    path('pets/<int:id>/',views.pets_put),
-    path('pets/<int:id>/',views.pets_delete)
+    path('pets/<int:id>/update',views.pets_put),
+    path('pets/<int:id>/delete',views.pets_delete),
+    path('pets/tratamientos/dosis-total',views.pets_daily_dose),
+    path('pets/mascotas/control-peso',views.pets_weight_control)
 ]
--- pets/views/pets.py
+++ pets/views/pets.py
@@ -2,6 +2,9 @@ from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 from pets.models import Pets
 from pets.serializers.pets import PetsSerializer
@@ -35,25 +38,67 @@ def pets_get_by_id(request, id: int):
     return Response(PetsSerializer(pet).data, status=status.HTTP_200_OK)
 
 @api_view(["PUT"])
-def pets_put(request, id: int):
-    print("Ingresa")
-    
+def pets_put(request, id: int):    
     try:
         pet = Pets.objects.get(pk=id)
     except Pets.DoesNotExist:
         return Response({'Detail': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
-    serializer = PetsSerializer(isinstance=pet, data=request.data)
-    if serializer.is_valid:
+    serializer = PetsSerializer(instance=pet, data=request.data)
+    if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["DELETE"])
 def pets_delete(request, id: int):
-    print("Ingresa")
     try:
         pet = Pets.objects.get(pk=id)
     except Pets.DoesNotExist:
         return Response({'Detail': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
     pet.delete()
     return Response({'Detail': 'Registro eliminado'}, status=status.HTTP_200_OK)
+
+@csrf_exempt
+def pets_daily_dose(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            daily_dose = data.get('daily_dose', [])
+            total_dose = 0
+            message = ""
+            for dose in daily_dose:
+                total_dose += dose
+                
+            if total_dose < 100:
+                message = "Tratamiento de baja intensidad"
+            elif total_dose < 300:
+                message = "Tratamiento moderado"
+            else:
+                message = "Tratamiento fuerte, seguir observación"
+                
+            return JsonResponse({"Dosis total": total_dose, "mensaje": message}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"Error": 'El dato requerido es una lista de dosis. Ej. ("daily_dose": [100,200,300])'}, status=400)
+    
+@csrf_exempt
+def pets_weight_control(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            current_weight = data.get('current_weight')
+            ideal_weight = data.get('ideal_weight')
+            weight_diff = 0
+            message = ""
+            
+            weight_diff = current_weight - ideal_weight
+            
+            if weight_diff > 0:
+                message = "La mascota está por encima del peso ideal"
+            elif weight_diff < 0:
+                message = "La mascota está por debajo del peso ideal"
+            else:
+                message = "Peso ideal alcanzado"
+                
+            return JsonResponse({"Diferencia de pesos": weight_diff, "mensaje": message}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"Error": 'El dato requerido es el peso ideal(kg) y el peso actual (kg). Ej. ("current_weight": 50, "ideal_weight": 60)'}, status=400)
